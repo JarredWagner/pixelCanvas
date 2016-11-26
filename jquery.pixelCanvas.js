524a73de89c1775bff0d990255918fd0ecd192f1
Jarred Wagner <jarred@jarredwagner.com>
2016-11-26 14:52:00 -0500

    v1.1
    Added orientation priority, cleaned up.

--- jquery.pixelCanvas.js
+++ jquery.pixelCanvas.js
@@ -2,81 +2,108 @@
 
   $.fn.pixelCanvas = function(options) {
 
-    var settings = $.extend({
-      divisor: 50,
-      opacity: 0.25
-    }, options);
+    //Settings
 
-    //Remove canvas and inline styles in event of resize
-    $(this).children('canvas').remove();
-    $(this).removeAttr('style');
+      var settings = $.extend({
+        priority: 'vertical',
+        pixelSize: 50,
+        minOpacity: 0,
+        maxOpacity: 0.25
+      }, options);
 
-    //Get this height, pre-canvas
-    var thisHeight = $(this).outerHeight();
+    //DOM
 
-    //Create unique ID
-    var canvasID = 'canvas'+Math.round(Math.random().toFixed(2)*100);
-    var canvasHash = '#' + canvasID
+      //Clean slate for resize or orientationchange
+      $(this).children('canvas').remove().removeAttr('style');
 
-    //Bring any children to the top
-    $(this).find('*').css('z-index', '777');
+      //Get this size, pre-canvas
+      var thisWidth = $(this).outerWidth();
+      var thisHeight = $(this).outerHeight();
 
-    //Insert canvas before children
-    $(this).prepend('<canvas id="'+canvasID+'"><canvas>');
+      //Set relative position for animation support, bring any children to the top
+      $(this).css('position', 'relative').find('*').css('z-index', '777');
 
-    //Set position to relative, so absolutely positioned canvas moves with nav animation
-    $(this).css('position', 'relative');
-    //Position canvas
-    $(canvasHash).css('position', 'absolute').css('top', 0).css('left', 0);
-    //Fill width
-    $(canvasHash).attr('width', $(window).width());
+      //Create unique ID
+      var canvasID = 'canvas'+Math.round(Math.random().toFixed(2)*100);
+      var canvasHash = '#' + canvasID
 
-    //Set pixel size
-    var canvasWidth = $(canvasHash).width();
-    var canvasHeight = thisHeight;
-    var squareSize = canvasHeight/settings.divisor;
+      //Insert Canvas
+      $(this).prepend('<canvas id="'+canvasID+'"><canvas>');
+      //Set size, position
+      $(canvasHash)
+        .attr('width', thisWidth)
+        .attr('height', thisHeight)
+        .css('position', 'absolute')
+        .css('top', 0)
+        .css('left', 0)
+      ;
 
-    //Find row amount
-    var columns = canvasWidth/squareSize;
+    //Drawing function
 
-    //Set canvas height
-    $(canvasHash).attr('height', thisHeight);
+      function drawPixels(iLoopParameter, jLoopParameter, x, y) {
 
-    //Draw pixels!
-    var canvas = document.getElementById(canvasID);
-    if (canvas === null) {
-      return;
-    }
-    var ctx = canvas.getContext('2d');
-
-    var i = 0;
-
-    //Draw rows
-    while ( i < settings.divisor ) {
-
-      var j = 0;
-
-      //Draw columns
-      while ( j < columns ) {
-
-        //Randomize
-        var randomOpacity = Math.random().toFixed(2);
-        //Cap opacity
-        while (randomOpacity > settings.opacity) {
-          randomOpacity = Math.random().toFixed(2);
+        var canvas = document.getElementById(canvasID);
+        if (canvas === null) {
+          return;
         }
-        var randomColor = parseInt(Math.random()*100);
+        var ctx = canvas.getContext('2d');
 
-        ctx.fillStyle = "rgba("+ randomColor +","+ randomColor +","+ randomColor +","+ randomOpacity +")";
-        ctx.fillRect(squareSize * j, squareSize * i, squareSize, squareSize);
+        var i = 0;
+        while ( i < iLoopParameter ) {
 
-        j++;
+          var j = 0;
+          while ( j < jLoopParameter ) {
+
+            //Randomize Opacity
+            var randomOpacity = Math.random().toFixed(2);
+            //Constrain Opacity
+            while (randomOpacity < settings.minOpacity || randomOpacity > settings.maxOpacity) {
+              randomOpacity = Math.random().toFixed(2);
+            }
+            //Randomize Color
+            var randomColor = parseInt(Math.random()*100);
+
+            ctx.fillStyle = "rgba("+ randomColor +","+ randomColor +","+ randomColor +","+ randomOpacity +")";
+            
+            if ( settings.priority === 'vertical' ) {
+							var x = j;
+							var y = i;
+			      } else if ( settings.priority === 'horizontal' ) {
+							var x = i;
+							var y = j;		
+			      }
+			      
+            ctx.fillRect(iLoopParameter * x, iLoopParameter * y, iLoopParameter, iLoopParameter);
+
+            j++;
+
+          }
+
+          i++;
+
+        }
 
       }
 
-      i++;
+    //Prep pixels and draw
 
-    }
+      if ( settings.priority === 'vertical' ) {
+
+        //VERTICAL PRIORITY
+
+          var rows = thisHeight/settings.pixelSize;
+          var columns = thisWidth/rows;
+          drawPixels(rows, columns);
+
+      } else if ( settings.priority === 'horizontal' ) {
+
+        //HORIZONTAL PRIORITY
+
+          var columns = thisWidth/settings.pixelSize;
+          var rows = thisHeight/columns;
+          drawPixels(columns, rows);
+
+      }
 
     return this;
 
